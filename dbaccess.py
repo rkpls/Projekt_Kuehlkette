7e87cd681f9618763eb85c06e9b2fb14a545a83a
rkpls <142883593+rkpls@users.noreply.github.com>
2024-08-13 10:25:27 +0200

    Create dbaccess.py

---
+++ dbaccess.py
@@ -0,0 +1,26 @@
+import pyodbc
+# Verbindungsdaten
+server = 'sc-db-server.database.windows.net'
+database = 'supplychain' # Setze den Namen deiner Datenbank hier ein
+username = 'rse'
+password = 'Pa$$w0rd'
+# Verbindungsstring
+conn_str = (
+    f'DRIVER={{ODBC Driver 17 for SQL Server}};'
+    f'SERVER={server};'
+    f'DATABASE={database};'
+    f'UID={username};'
+    f'PWD={password}'
+)
+# Verbindung herstellen
+conn = pyodbc.connect(conn_str)
+# Cursor erstellen
+cursor = conn.cursor()
+# SQL-Statement ausführen
+cursor.execute('SELECT * FROM coolchain1')
+# Ergebnisse ausgeben
+for row in cursor:
+    print(row)
+# Verbindung schließen
+cursor.close()
+conn.close()
