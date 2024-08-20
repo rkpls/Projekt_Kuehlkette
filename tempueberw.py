a293b6352c7ffc2170b7a98181b8241c7b28c266
rkpls <142883593+rkpls@users.noreply.github.com>
2024-08-20 10:18:15 +0200

    1

---
+++ tempueberw.py
@@ -0,0 +1,7 @@
+"""
+Programmabschnitt Temperaturüberwachung der Kühlstationen
+Aufgabe 1.1
+Version 0
+20.8.2024
+Bearbeiter: 
+"""
---
+++ wetterdaten.py
@@ -0,0 +1,7 @@
+"""
+Programmabschnitt Wetterdatenabfrage an den Auslagerorten
+Aufgabe 1.1
+Version 0
+20.8.2024
+Bearbeiter: 
+"""
