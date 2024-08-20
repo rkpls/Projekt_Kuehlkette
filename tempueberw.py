2d136a014e4ff7ad1767ddea9a40e70032742378
rkpls <142883593+rkpls@users.noreply.github.com>
2024-08-20 10:56:14 +0200

    V1.0 temperaturabfrage

--- tempueberw.py
+++ tempueberw.py
@@ -1,12 +1,12 @@
 """
 Programmabschnitt Temperaturüberwachung der Kühlstationen
 Aufgabe 1.2
-Version 0
+Version 1.0
 20.8.2024
-Bearbeiter: 
+Bearbeiter: Lars 
 """
 
-
+import time
 import pyodbc
     # Verbindungsdaten
 server = 'sc-db-server.database.windows.net'
@@ -23,20 +23,32 @@ conn_str = (
 )
 
 while True:
-        # Verbindung herstellen
-    conn = pyodbc.connect(conn_str)
-        # Cursor erstellen
-    cursor = conn.cursor()
-        # SQL-Statement ausführen
-    cursor.execute('SELECT * FROM coolchain1')
+    try:
+        conn = pyodbc.connect(conn_str)
+        cursor = conn.cursor()
+        cursor.execute('SELECT transportstationID, temperature FROM tempdata')
 
-    transport_id = input("Enter transport ID: ")
-    cursor.execute('SELECT * FROM coolchain1 WHERE transportid = ?', (transport_id,))
-    row = cursor.fetchone()
-    if temp:
-        print(row)
-    else:
-        print("No row found with the given transport ID.")
-    cursor.close()
-    conn.close()
+        rows = cursor.fetchall()
+        
+        for row in rows:
+            temp = row.temperature
+            id = row.transportstationID
+
+            if temp > 4:
+                idhigh = id
+            elif temp < 2:
+                idlow = id    
+            else:
+                pass
+            
+        print(f"Temperatur überschritten bei ID: {idhigh}")
+        print(f"Temperatur unterschritten bei ID: {idlow}")
+                
+    except pyodbc.Error as e:
+        print(f"Database error occurred: {e}")
+    finally:
+        cursor.close()
+        conn.close()
+        
+    time.sleep(900)
 
