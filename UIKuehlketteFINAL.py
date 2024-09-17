79219016994fb356b37ebc43fe6640db5d140692
rkpls <142883593+rkpls@users.noreply.github.com>
2024-09-17 12:35:23 +0200

    python bug mit arab sprache unfixed

--- UIKuehlketteFINAL.py
+++ UIKuehlketteFINAL.py
@@ -145,7 +145,8 @@ def display_results(results, transport_id):
             delta_to_present = datetime.now() - last_datetime
             days = delta_to_present.days
             hours = delta_to_present.seconds // 3600
-            final_error_label = ctk.CTkLabel(frame_results, text=lang ["Lieferung nicht vollständig. Zeit seit letztem Eintrag"] +":"+ f"{days}d {hours}h", font=("Arial", 14, "bold"), text_color="red")
+            delta = str(f" {days}d {hours}h")
+            final_error_label = ctk.CTkLabel(frame_results, text=lang ["Lieferung nicht vollständig. Zeit seit letztem Eintrag: "] + delta, font=("Arial", 14, "bold"), text_color="red")
             final_error_label.grid(row=row_index + 2, column=5, columnspan=1, pady=0)
 
     else:
@@ -206,7 +207,7 @@ LANGUAGES = {
         "Fehler": "Fehler",
         "Bitte eine valide Transport ID eingeben.": "Bitte eine valide Transport ID eingeben.",
         "Fehler bei Datenbankzugriff. Netzwerkverbindung prüfen.": "Fehler bei Datenbankzugriff. Netzwerkverbindung prüfen.",
-        "Lieferung nicht vollständig. Zeit seit letztem Eintrag": "Lieferung nicht vollständig. Zeit seit letztem Eintrag:"
+        "Lieferung nicht vollständig. Zeit seit letztem Eintrag: ": "Lieferung nicht vollständig. Zeit seit letztem Eintrag: "
     },
     "EN": {
         "Transport ID eingeben:": "Enter Transport ID:",
@@ -226,7 +227,7 @@ LANGUAGES = {
         "Fehler": "Error",
         "Bitte eine valide Transport ID eingeben.": "Please enter a valid Transport ID.",
         "Fehler bei Datenbankzugriff. Netzwerkverbindung prüfen.": "Database access error. Please check your network connection.",
-        "Lieferung nicht vollständig. Zeit seit letztem Eintrag": "Delivery incomplete. Time since last entry"
+        "Lieferung nicht vollständig. Zeit seit letztem Eintrag: ": "Delivery incomplete. Time since last entry: "
     },
     "AR": {
         "Transport ID eingeben:": "أدخل معرف النقل:",
@@ -246,7 +247,7 @@ LANGUAGES = {
         "Fehler": "خطأ",
         "Bitte eine valide Transport ID eingeben.": "يرجى إدخال معرف نقل صالح.",
         "Fehler bei Datenbankzugriff. Netzwerkverbindung prüfen.": "خطأ في الوصول إلى قاعدة البيانات. يرجى التحقق من اتصال الشبكة.",
-        "Lieferung nicht vollständig. Zeit seit letztem Eintrag": "التسليم غير مكتمل. الوقت منذ آخر تسجيل"
+        "Lieferung nicht vollständig. Zeit seit letztem Eintrag: ": "التسليم غير مكتمل. الوقت منذ آخر تسجيل: "
     }
 }
 
